a6d5871b867a11d71800d426f6202f2418ab3428
khanshorif331 <khanshorif331@gmail.com>
2022-05-10 22:58:06 +0600

    added navbar and router setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import About from './Pages/About/About'
+import Appointment from './Pages/Appointment/Appointment'
+import ContactUs from './Pages/ContactUs/ContactUs'
+import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
+import NotFound from './Pages/NotFound/NotFound'
+import Reviews from './Pages/Reviews/Reviews'
+import Navbar from './Pages/Shared/Navbar'
 
 function App() {
 	return (
 		<div>
-			<h1>Doctors Portal</h1>
-			<button class='btn btn-secondary'>Button</button>
+			<Navbar></Navbar>
+			<Routes>
+				<Route path='/' element={<Home></Home>}></Route>
+				<Route path='/home' element={<Home></Home>}></Route>
+				<Route path='/about' element={<About></About>}></Route>
+				<Route
+					path='/appointment'
+					element={<Appointment></Appointment>}
+				></Route>
+				<Route path='/reviews' element={<Reviews></Reviews>}></Route>
+				<Route path='/contactus' element={<ContactUs></ContactUs>}></Route>
+				<Route path='/login' element={<Login></Login>}></Route>
+				<Route path='/*' element={<NotFound></NotFound>}></Route>
+			</Routes>
 		</div>
 	)
 }
---
+++ src/Pages/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reviews = () => {
+	return (
+		<div>
+			<h1>This is review page.</h1>
+		</div>
+	)
+}
+
+export default Reviews
