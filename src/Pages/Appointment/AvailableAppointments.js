c58512e6087e1a75f8cbbdce56c8a63278e555a4
khanshorif331 <khanshorif331@gmail.com>
2022-05-13 01:10:42 +0600

    appointment section added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,12 +1,25 @@
 import { format } from 'date-fns'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Service from './Service'
 
 const AvailableAppointments = ({ date }) => {
+	const [services, setServices] = useState([])
+	useEffect(() => {
+		fetch('services.json')
+			.then(res => res.json())
+			.then(data => setServices(data))
+	}, [])
+
 	return (
 		<div>
 			<h1 className='text-xl text-secondary text-center'>
 				Available Appointments on : {format(date, 'PP')}
 			</h1>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+				{services.map(service => (
+					<Service key={service._id} service={service}></Service>
+				))}
+			</div>
 		</div>
 	)
 }
---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Service = ({ service }) => {
+	const { name, slots } = service
+	return (
+		<div class='card lg:max-w-lg bg-base-100 shadow-xl'>
+			<div class='card-body'>
+				<h2 class='card-title'>{name}</h2>
+				<p>
+					{slots.length > 0 ? (
+						<span>{slots[0]}</span>
+					) : (
+						<span className='text-red-500'>No slot available</span>
+					)}
+				</p>
+				<p>
+					{' '}
+					{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available{' '}
+				</p>
+				<div class='card-actions justify-end'>
+					<button class='btn btn-primary'>Buy Now</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Service
