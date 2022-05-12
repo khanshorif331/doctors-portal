f4edd6a8a28c97e83f28655525e1198fca10e875
khanshorif331 <khanshorif331@gmail.com>
2022-05-13 01:37:35 +0600

    modal added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,9 +1,13 @@
 import { format } from 'date-fns'
 import React, { useEffect, useState } from 'react'
+import BookingModal from './BookingModal'
 import Service from './Service'
 
 const AvailableAppointments = ({ date }) => {
 	const [services, setServices] = useState([])
+
+	const [treatment, setTreatment] = useState(null)
+
 	useEffect(() => {
 		fetch('services.json')
 			.then(res => res.json())
@@ -17,9 +21,14 @@ const AvailableAppointments = ({ date }) => {
 			</h1>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 				{services.map(service => (
-					<Service key={service._id} service={service}></Service>
+					<Service
+						key={service._id}
+						service={service}
+						setTreatment={setTreatment}
+					></Service>
 				))}
 			</div>
+			{treatment && <BookingModal treatment={treatment}></BookingModal>}
 		</div>
 	)
 }
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,25 +1,29 @@
 import React from 'react'
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
 	const { name, slots } = service
 	return (
 		<div class='card lg:max-w-lg bg-base-100 shadow-xl'>
 			<div class='card-body'>
-				<h2 class='card-title'>{name}</h2>
+				<h2 class='card-title text-secondary'>{name}</h2>
 				<p>
 					{slots.length > 0 ? (
 						<span>{slots[0]}</span>
 					) : (
-						<span className='text-red-500'>No slot available</span>
+						<span className='text-red-400'>Try another date.!</span>
 					)}
 				</p>
 				<p>
 					{' '}
 					{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available{' '}
 				</p>
-				<div class='card-actions justify-end'>
-					<button disabled={slots.length === 0} class='btn btn-primary'>
-						Buy Now
+				<div class='card-actions justify-center'>
+					<button
+						onClick={() => setTreatment(service)}
+						disabled={slots.length === 0}
+						class='btn btn-secondary text-white uppercase cursor-pointer'
+					>
+						Book Appointment
 					</button>
 				</div>
 			</div>
