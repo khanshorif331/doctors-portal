81c4dd3e8a9fc0f5e2f0177d545a8b973a643680
khanshorif331 <khanshorif331@gmail.com>
2022-05-14 19:57:39 +0600

    boookking data is ready too send in backeend

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -7,12 +7,24 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
 	const { name, slots, _id } = treatment
 
 	const [user, loading, error] = useAuthState(auth)
+	const formattedDate = format(date, 'PP')
 
 	const handleBooking = event => {
 		event.preventDefault()
 		const slot = event.target.slot.value
 
 		console.log(_id, name, slot)
+
+		const booking = {
+			treatmentId: _id,
+			treatment: name,
+			date: formattedDate,
+			slot,
+			patient: user.email,
+			patientName: user.displayName,
+			phone: event.target.phone.value,
+		}
+
 		setTreatment(null)
 	}
 	return (
