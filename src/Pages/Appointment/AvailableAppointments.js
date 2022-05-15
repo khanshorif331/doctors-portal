7b64ca9f33fe55fbce7973cf7d94787d16a03f6a
khanshorif331 <khanshorif331@gmail.com>
2022-05-15 18:07:54 +0600

    added react query

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,20 +1,26 @@
 import { format } from 'date-fns'
 import React, { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
+import Loading from '../Shared/Loading'
 import BookingModal from './BookingModal'
 import Service from './Service'
 
 const AvailableAppointments = ({ date }) => {
-	const [services, setServices] = useState([])
-
 	const [treatment, setTreatment] = useState(null)
-
 	const formattedDate = format(date, 'PP')
 
-	useEffect(() => {
-		fetch(`http://localhost:5000/available?date=${formattedDate}`)
-			.then(res => res.json())
-			.then(data => setServices(data))
-	}, [])
+	const {
+		data: services,
+		isLoading,
+		refetch,
+	} = useQuery(['available', formattedDate], () =>
+		fetch(`http://localhost:5000/available?date=${formattedDate}`).then(res =>
+			res.json()
+		)
+	)
+	if (isLoading) {
+		return <Loading></Loading>
+	}
 
 	return (
 		<div>
@@ -22,7 +28,7 @@ const AvailableAppointments = ({ date }) => {
 				Available Appointments on : {format(date, 'PP')}
 			</h1>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-				{services.map(service => (
+				{services?.map(service => (
 					<Service
 						key={service._id}
 						service={service}
@@ -35,6 +41,7 @@ const AvailableAppointments = ({ date }) => {
 					date={date}
 					treatment={treatment}
 					setTreatment={setTreatment}
+					refetch={refetch}
 				></BookingModal>
 			)}
 		</div>
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init'
 import { toast } from 'react-toastify'
 
-const BookingModal = ({ treatment, date, setTreatment }) => {
+const BookingModal = ({ treatment, date, setTreatment, refetch }) => {
 	const { name, slots, _id } = treatment
 
 	const [user, loading, error] = useAuthState(auth)
@@ -44,6 +44,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
 						`You already have an appointment on ${data.booking?.date} at ${data.booking?.slot}`
 					)
 				}
+				refetch()
 				setTreatment(null)
 			})
 	}
