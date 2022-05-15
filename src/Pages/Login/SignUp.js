f21c14a13bc2990fd560dc7e9ed0d7d93019e6c9
khanshorif331 <khanshorif331@gmail.com>
2022-05-16 02:08:19 +0600

    added token and signup component

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -8,6 +8,7 @@ import auth from '../../firebase.init'
 import { useForm } from 'react-hook-form'
 import Loading from '../Shared/Loading'
 import { Link, useNavigate } from 'react-router-dom'
+import useToken from '../../hooks/useToken'
 
 const SignUp = () => {
 	const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth)
@@ -16,6 +17,8 @@ const SignUp = () => {
 		useCreateUserWithEmailAndPassword(auth)
 
 	const [updateProfile, updating, updateError] = useUpdateProfile(auth)
+	const [token] = useToken(user || gUser)
+
 	const navigate = useNavigate()
 
 	const {
@@ -30,7 +33,7 @@ const SignUp = () => {
 		await createUserWithEmailAndPassword(data.email, data.password)
 		await updateProfile({ displayName: data.name })
 		console.log('update done')
-		navigate('/appointment')
+		// navigate('/appointment')
 	}
 
 	if (loading || gLoading || updating) {
