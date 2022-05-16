7b1e79f35b7879b0578eaa61c5c7f2900c34e9d3
khanshorif331 <khanshorif331@gmail.com>
2022-05-17 00:43:53 +0600

    made an admin dashboard

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,13 +1,42 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 
-const UserRow = ({ user }) => {
-	const { email } = user
+const UserRow = ({ user, refetch }) => {
+	const { email, role } = user
+	const makeAdmin = () => {
+		fetch(`http://localhost:5000/user/admin/${email}`, {
+			method: 'PUT',
+			headers: {
+				authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+			},
+		})
+			.then(res => {
+				if (res.status === 403) {
+					toast.error('Failed to make an admin')
+				}
+				return res.json()
+			})
+			.then(data => {
+				if (data.modifiedCount > 0) {
+					refetch()
+					toast.success('Successfully made admin')
+				}
+			})
+	}
 	return (
 		<tr>
 			<th>1</th>
 			<td>{email}</td>
-			<td>Quality Control Specialist</td>
-			<td>Blue</td>
+			<td>
+				{role !== 'admin' && (
+					<button onClick={makeAdmin} class='btn btn-xs '>
+						Make Admin
+					</button>
+				)}
+			</td>
+			<td>
+				<button class='btn btn-xs'>Remove User</button>
+			</td>
 		</tr>
 	)
 }
