4ffd24a40edef96ac48946158a56897914b9cfc7
khanshorif331 <khanshorif331@gmail.com>
2022-05-18 20:33:43 +0600

    created manage doctoors component

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import MyHistory from './Pages/Dashboard/MyHistory'
 import Users from './Pages/Dashboard/Users'
 import RequireAdmin from './Pages/Login/RequireAdmin'
 import AddDoctor from './Pages/Dashboard/AddDoctor'
+import ManageDoctors from './Pages/Dashboard/ManageDoctors'
 
 function App() {
 	return (
@@ -60,6 +61,14 @@ function App() {
 						element={
 							<RequireAdmin>
 								<AddDoctor></AddDoctor>
+							</RequireAdmin>
+						}
+					></Route>
+					<Route
+						path='manageDoctor'
+						element={
+							<RequireAdmin>
+								<ManageDoctors></ManageDoctors>
 							</RequireAdmin>
 						}
 					></Route>
---
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManageDoctors = () => {
+	return (
+		<div>
+			<h1 className='text-2xl'>Manage Doctors</h1>
+		</div>
+	)
+}
+
+export default ManageDoctors
